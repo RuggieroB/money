aa3c312286f82c325ec34f8e542e6c48ced652f9
EnricoRuggieroB <1bmeccenricorugbucchianico@gmail.com>
2019-02-07 11:45:19 +0000

    Algoritmo di Laboratorio di Programmazione. Alunno: Bucchianico Enrico Ruggiero, 4^Finf.

--- money.py
+++ money.py
@@ -14,11 +14,11 @@ if imp < 0:
 	print(" \n ERRORE! L'importo non puo' essere negativo!")
 else:
 	venti = imp // 20
-	imp -= venti * 20
+	imp = imp % 20
 	dieci = imp // 10
-	imp -= dieci * 10
+	imp = imp % 10
 	cinque = imp // 5
-	imp -= cinque * 5
+	imp = imp % 5
 	uno = imp
 	print(" \n Banconote da 20$=", venti, "; \n Banconote da 10$=", dieci, "; \n Banconote da  5$=", cinque, "; \n "\
 	      "Banconote da  1$:", uno,".")
